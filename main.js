61071afcfcdab6d59035a029c4bed93c87ae85ab
Chaya <Chaya.B.Milstein@gmail.com>
2023-04-11 00:39:03 +0100

    tests written with solution code

--- main.js
+++ main.js
@@ -5,10 +5,23 @@ There are aliens that have contacted us, and they have tried to use English, but
 
 The good news is that the alien's attempt is really similar to English, but all the letters they use are 5 letters after the real words in the alphabet. So, for example, the word "hello" would be "mjqqt".
 
-write a function that takes a string and returns the alien translation!
+Any letter at the end of the alphabet will wrap around to the beginning, so "z" would be "e".
+
+write a function that takes an alien string and returns the English translation!
+(you will only get a string with lowercase letters and spaces).
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
-function alienTranslator(string) {
-    
+export function alienTranslator(string) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let alienAlphabet = 'fghijklmnopqrstuvwxyzabcde';
+    let translation = '';
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === ' ') {
+            translation += ' ';
+        } else {
+        translation += alphabet[alienAlphabet.indexOf(string[i])];
+        }
+    }
+    return translation;
 }
--- main.test.js
+++ main.test.js
@@ -1,1 +1,18 @@
 //👉 Write your tests below here:
+import { test, expect } from '@jest/globals';
+import { alienTranslator } from './main.js';
+
+//test easiest case - middle of alphabet
+test('correct translation middle of alphabet', () => {
+    expect(alienTranslator('mjqqt')).toBe('hello');
+});
+
+//test end of alphabet (wrap around)
+test('correct translation end of alphabet', () => {
+    expect(alienTranslator('e')).toBe('z');
+});
+
+//test spaces
+test('correct translation with spaces', () => {
+    expect(alienTranslator('mjqqt e')).toBe('hello z');
+});
