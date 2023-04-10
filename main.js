1cf4f23c98bb4c0a4caa45104191c247fd47bb71
Chaya <Chaya.B.Milstein@gmail.com>
2023-04-10 23:38:19 +0100

    Wrote the kata

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 /* 
-👉 Write your kata here!
+alien language translator
+
+There are aliens that have contacted us, and they have tried to use English, but it has gone a bit wrong!
+
+The good news is that the alien's attempt is really similar to English, but all the letters they use are 5 letters after the real words in the alphabet. So, for example, the word "hello" would be "mjqqt".
+
+write a function that takes a string and returns the alien translation!
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function alienTranslator(string) {
+    
+}
