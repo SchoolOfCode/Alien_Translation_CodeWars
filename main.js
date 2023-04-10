4235ed883babc38f14429c08519d1e7654329446
Chaya <Chaya.B.Milstein@gmail.com>
2023-04-11 00:53:41 +0100

    code and tests with no solution code

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ alien language translator
 
 There are aliens that have contacted us, and they have tried to use English, but it has gone a bit wrong!
 
-The good news is that the alien's attempt is really similar to English, but all the letters they use are 5 letters after the real words in the alphabet. So, for example, the word "hello" would be "mjqqt".
+The good news is that the alien's attempt is really similar to English, but all the letters they use are 5 letters after the real letters in the alphabet. So, for example, the word "hello" would be "mjqqt". (The 5th letter after "h" is "m" , the 5th letter after "e" is "j", etc.)
 
 Any letter at the end of the alphabet will wrap around to the beginning, so "z" would be "e".
 
@@ -13,15 +13,5 @@ write a function that takes an alien string and returns the English translation!
 
 //👉 Write the function your CodeWarriors will start with below here:
 export function alienTranslator(string) {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let alienAlphabet = 'fghijklmnopqrstuvwxyzabcde';
-    let translation = '';
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === ' ') {
-            translation += ' ';
-        } else {
-        translation += alphabet[alienAlphabet.indexOf(string[i])];
-        }
-    }
-    return translation;
+    //your code here - good luck! Or rather "ltti qzhp!"
 }
